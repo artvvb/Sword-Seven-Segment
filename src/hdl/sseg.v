a4ba8eb3e47d78aa3bf514f02db310eef4b675c0
Arthur Brown <artvvb@gmail.com>
2017-11-20 10:40:05 -0800

    Documented src files

--- src/hdl/sseg.v
+++ src/hdl/sseg.v
@@ -1,17 +1,14 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
+// Company: Digilent
+// Engineer: Arthur Brown
 // 
 // Create Date: 11/16/2017 04:10:17 PM
-// Design Name: 
 // Module Name: sseg
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
+// Project Name: Sword Seven Segment
+// Target Devices: Sword Board
+// Tool Versions: Vivado 2016.4
+// Description: implements a controller for the seven segment shift registers of the sword board.
 // 
 // Revision:
 // Revision 0.01 - File Created
@@ -20,30 +17,33 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module sseg(clk, din, ss_dout, ss_clk, ss_en);
-    parameter DBG="FALSE";
-    input clk;
-    input [31:0] din;
-    output wire ss_dout, ss_clk, ss_en;
-    localparam COUNT_MAX = (DBG == "FALSE") ? 199999 : 19;
-    localparam BIT_PERIOD = (DBG == "FALSE") ? 4 : 4;
-    localparam BIT_PERIOD_HALF= (DBG == "FALSE") ? 2 : 2;
+module sseg(clk, din, ss_sdo, ss_clk, ss_en);
+    parameter DBG="FALSE"; // enable debug mode for simulation, reduces wait time between programming sequences
+    
+    input wire clk;
+    input wire [31:0] din;
+    output wire ss_sdo, ss_clk, ss_en; // seven segment control pins, shift-data-out, clock, enable
+    
+    localparam COUNT_MAX = (DBG == "FALSE") ? 199999 : 19; // when not in debug mode, wait 1ms between shifting periods
+    localparam BIT_PERIOD = (DBG == "FALSE") ? 4 : 4; // wait time for each bit to be shifted
+    localparam BIT_PERIOD_HALF = BIT_PERIOD / 2;
+    
     reg [31:0] count = 0;
     reg [5:0] bit_count;
     reg idle = 1;
     assign ss_en = idle;
     always@(posedge clk)
-        if (idle == 1) begin
+        if (idle == 1) begin // count up to OCUNT_MAX, then begin programming sequence
             if (count == COUNT_MAX) begin
                 bit_count <= 0;
                 idle <= 0;
                 count <= 0;
             end else
                 count <= count + 1;
-        end else begin
+        end else begin // shift out 64 bits, waiting BIT_PERIOD clocks on each bit
             if (count == BIT_PERIOD-1) begin
                 count <= 0;
-                if (bit_count == 6'h3F)
+                if (bit_count == 6'h3F) // finish programming if at end of last bit
                     idle <= 1;
                 else
                     bit_count <= bit_count + 1;
@@ -51,10 +51,16 @@ module sseg(clk, din, ss_dout, ss_clk, ss_en);
                 count <= count + 1;
             end
         end
+    
+    // hold clock high when idle, pull it low for the start of each bit period, data is clocked out on positive edge and needs a 5ns setup time
     assign ss_clk = (idle == 1 || count >= BIT_PERIOD_HALF) ? 1 : 0;
+    
+    // select 4 bits from input
     reg [3:0] nibble;
     always@(*)
         nibble = 4'hF & (din >> {bit_count[5:3], 2'b0});
+        
+    // choose 7seg display pattern for selected bits
     reg [7:0] seg;
     always@(*) begin
         case (nibble)
@@ -76,5 +82,7 @@ module sseg(clk, din, ss_dout, ss_clk, ss_en);
         4'hF: seg = 8'b01110001;
         endcase
     end
-    assign ss_dout = seg[~bit_count[2:0]];
+    
+    // shift out selected display pattern high to low
+    assign ss_sdo = seg[~bit_count[2:0]];
 endmodule
--- src/hdl/top.v
+++ src/hdl/top.v
@@ -1,17 +1,14 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
+// Company: Digilent
+// Engineer: Arthur Brown
 // 
 // Create Date: 11/17/2017 09:31:07 AM
-// Design Name: 
 // Module Name: top
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
+// Project Name: Sword Seven Segment
+// Target Devices: Sword Board
+// Tool Versions: Vivado 2016.4
+// Description: wraps simple seven segment controller for the seven segment circuitry of the sword board
 // 
 // Revision:
 // Revision 0.01 - File Created
@@ -20,11 +17,23 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module top (clk_p, clk_n, ss_dout, ss_clk, ss_en);
-    parameter DBG = "FALSE";
-    input  wire clk_p, clk_n;
-    output wire ss_dout, ss_clk, ss_en;
-    wire clk;
-    clk_wiz_0 wiz (.clk_in1_p(clk_p), .clk_in1_n(clk_n), .clk_out1(clk));
-    sseg #(DBG) uut (clk, 32'h01234567, ss_dout, ss_clk, ss_en);
+module top (clk_p, clk_n, ss_sdo, ss_clk, ss_en);
+    parameter DBG = "FALSE"; // set to TRUE to use in simulator, significantly reduces wait periods
+    input wire clk_p, clk_n; // 200MHz differential input clock
+    output wire ss_sdo, ss_clk, ss_en; // seven segment display control pins
+    wire clk; // 200MHz single ended clock
+    clk_wiz_0 get_clk (
+        .clk_in1_p(clk_p),
+        .clk_in1_n(clk_n),
+        .clk_out1(clk)
+    );
+    sseg #(
+        .DBG(DBG)
+    ) uut (
+        .clk(clk),
+        .din(32'h01234567), // constantly display code 01234567
+        .ss_sdo(ss_sdo),
+        .ss_clk(ss_clk),
+        .ss_en(ss_en)
+    );
 endmodule
